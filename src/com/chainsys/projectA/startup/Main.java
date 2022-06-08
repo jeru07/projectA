04b91df9cf738563aabf0c82a4d7dbb40885c71b
jeru07 <jerushaciana719@gmail.com>
2022-06-08 17:46:02 +0530

    add bean files

--- src/com/chainsys/projectA/startup/Main.java
+++ src/com/chainsys/projectA/startup/Main.java
@@ -1,10 +1,33 @@
 package com.chainsys.projectA.startup;
 
+import com.chainsys.projectA.beans.Customer;
+import com.chainsys.projectA.beans.Employee;
+import org.springframework.context.ApplicationContext; //parent
+import org.springframework.context.support.ClassPathXmlApplicationContext; //child
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		testB();
 	}
-
+  public static void testA()
+  {
+	  Employee emp=new Employee();
+	  emp.setId(1);
+	  emp.setName("jerusha");
+	  emp.print();
+  }
+  public static void testB()
+  {
+	  ApplicationContext ac=new ClassPathXmlApplicationContext("beans.xml");
+	  // Employee emp=ac.getBean(Employee.class);
+	  Employee emp=(Employee)ac.getBean("emp");
+	  emp.setId(7);
+	  emp.setName("virat");
+	  emp.print();
+	  Customer c=ac.getBean(Customer.class);
+	  c.setId(100);
+	  c.setName("dhoni");
+	  c.print();
+  }
 }
